30eaf2552e0ed751abba13ffe98ed69f8b71129c
ORPHEUS\Jedz1 <carlandrewv@yahoo.com>
2023-08-16 22:35:06 +0800

    finding maximum value

--- test.cpp
+++ test.cpp
@@ -1,8 +1,22 @@
+// Programming projects I've done for my school
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "hello world";
-    return 0;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=0; i<n; i++){
+        cin >> a[i]; 
+    }
+
+    int m = a[0];
+    for (int j=0; j<n; j++){
+        if (a[j]>m){
+            m = a[j];
+        }
+    }
+    cout << m;
 }
